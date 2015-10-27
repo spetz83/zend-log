be65330a048c076f6765f3cee5858fefff720f8d
Tom Etzel <thomas.etzel@gmail.com>
2015-10-26 23:08:31 -0500

    added constructor. Added getter/setter for encoding

--- test/Formatter/JsonTest.php
+++ test/Formatter/JsonTest.php
@@ -8,8 +8,28 @@
 
 namespace ZendTest\Log\Formatter;
 
+use Zend\Log\Formatter\Json as JsonFormatter;
+
 
 class JsonTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @dataProvider provideDateTimeFormats
+     */
+    public function testSetDateTimeFormat($dateTimeFormat)
+    {
+        $date = new \DateTime();
+        $formatter = new JsonFormatter();
+        $this->assertSame($formatter, $formatter->setDateTimeFormat($dateTimeFormat));
+        $this->assertContains($dateTimeFormat, $formatter->getDateTimeFormat());
+        $this->assertContains($date->format($dateTimeFormat), $formatter->format(['timestamp' => $date]));
+    }
 
+    public function provideDateTimeFormats()
+    {
+        return [
+            ['r'],
+            ['U'],
+        ];
+    }
 }
